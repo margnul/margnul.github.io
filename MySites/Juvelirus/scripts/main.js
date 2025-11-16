ed07a3447bc8c29ed1c1ce98d5879c45935654fb
margnul <ivan.meynart@gmail.com>
2025-11-16 15:50:29 +0300

    пофиксил подробный рассмотр картинки товара

--- MySites/Juvelirus/scripts/main.js
+++ MySites/Juvelirus/scripts/main.js
@@ -1,5 +1,22 @@
-import Cart from "./Cart.js";
-import Product from "./Product.js";
+// import Cart from "./Cart.js";
+// import Product from "./Product.js";
 
-new Cart()
-new Product()
+// new Cart()
+// new Product()
+
+// Получаем текущую страницу
+const pageName = document.body.dataset.page;
+
+// Cart загружается только на нужных страницах
+if (pageName === "cart" ) {
+  import("./Cart.js").then(({ default: Cart }) => {
+    new Cart();
+  });
+}
+
+// Product загружается только на странице товара
+if (pageName === "product") {
+  import("./Product.js").then(({ default: Product }) => {
+    new Product();
+  });
+}
